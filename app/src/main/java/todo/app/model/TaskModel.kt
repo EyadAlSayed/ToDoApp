95aca3ac48391fc6b3b11f5c8718431004c7e6cf
Eyad Al Sayed <eyadan.900@gmail.com>
2021-09-30 19:31:59 +0300

    first version

--- app/src/main/java/todo/app/model/TaskModel.kt
+++ app/src/main/java/todo/app/model/TaskModel.kt
@@ -2,8 +2,7 @@ package todo.app.model
 
 import java.io.Serializable
 
-
-class TaskModel(var id:String, val name: String, val description:String, val priority: Int, val date: String, val time: String):Serializable  {
+data class TaskModel(var id:String, val name: String, val description:String, val priority: Int, val date: String, val time: String):Serializable  {
 
     constructor():this("","","",0,"","")
 
