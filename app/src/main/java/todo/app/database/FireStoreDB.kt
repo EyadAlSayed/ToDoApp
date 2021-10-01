22bd0be01c5e5b448b5102ba453b5538ea82e3e6
Eyad Al Sayed <eyadan.900@gmail.com>
2021-10-01 09:21:38 +0300

    Done

--- app/src/main/java/todo/app/database/FireStoreDB.kt
+++ app/src/main/java/todo/app/database/FireStoreDB.kt
@@ -16,7 +16,7 @@ object FireStoreDB {
     fun getTaskCollectionRef() = FirebaseFirestore.getInstance().collection(collectionPath).document(userId!!).collection(subCollection)
     fun getQueryByLimit(limit: Long) = getTaskCollectionRef().orderBy("id").limit(limit)
     fun getQueryStartAfterLastVisibleDocWithLimit(field: String, fieldValue: String, limit: Long) = getTaskCollectionRef().orderBy(field).startAfter(fieldValue).limit(limit)
-    fun setTaskDoc(docID: String, task: TaskModel) = getTaskCollectionRef().document(docID).set(task)
+    fun setTaskDoc(docID: String, task: TaskModel) = getTaskCollectionRef().document(docID).set(task)  // it used to add and update the Task
     fun deleteDoc(docID: String) = getTaskCollectionRef().document(docID).delete()
 
 }
